100b7a1dd51bc961f9b731e58c04d94170323ad4
Andrey Valyaev <dron.valyaev@gmail.com>
2017-02-16 23:20:37 +0300

    #2: Key with KeyData

--- magma/DataImit.h
+++ magma/DataImit.h
@@ -1,15 +1,17 @@
 #include <memory>
 #include <vector>
 
-class MagmaKey;
+namespace magma {
+	class Key;
+}
 
 class MagmaDataImit {
 public:
-	MagmaDataImit(const std::vector<uint8_t> &data, const std::shared_ptr<const MagmaKey> &key);
+	MagmaDataImit(const std::vector<uint8_t> &data, const std::shared_ptr<const magma::Key> &key);
 
 	std::vector<uint8_t> value() const;
 	uint32_t asUInt32() const;
 private:
 	const std::vector<uint8_t> data;
-	const std::shared_ptr<const MagmaKey> key;
+	const std::shared_ptr<const magma::Key> key;
 };
--- magma/Key.h
+++ magma/Key.h
@@ -5,18 +5,21 @@
 
 class MagmaSbox;
 
-class MagmaKey {
+namespace magma {
+
+class KeyData;
+
+class Key {
 public:
-	MagmaKey(const std::vector<uint8_t> &key, const std::shared_ptr<const MagmaSbox> &sbox);
-	MagmaKey(const std::vector<uint32_t> &key, const std::shared_ptr<const MagmaSbox> &sbox);
+	Key(const std::shared_ptr<const KeyData> &key_data,
+		const std::shared_ptr<const MagmaSbox> &sbox);
 
 	std::vector<uint8_t> imit(const std::vector<uint8_t> &block) const;
-
 private:
-	static std::vector<uint32_t> fromBytes(const std::vector<uint8_t> &key);
-
 	uint32_t cycle(uint32_t v, int index) const;
 
-	const std::vector<uint32_t> key;
+	const std::shared_ptr<const KeyData> key_data;
 	const std::shared_ptr<const MagmaSbox> sbox;
 };
+
+}
