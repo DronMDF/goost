97063c7c6f345b8e293a93309d2a2dbf65fa7e58
Andrey Valyaev <dron.valyaev@gmail.com>
2017-03-27 23:05:50 +0300

    #19: Some clang-tidy cleanup

--- magma/SboxExpandedNibbleTab.h
+++ magma/SboxExpandedNibbleTab.h
@@ -7,7 +7,7 @@ class SboxExpandedNibbleTab final : public SboxExpanded::Tab {
 public:
 	SboxExpandedNibbleTab(const std::vector<int> &pl, const std::vector<int> &ph, int shift);
 
-	virtual uint32_t translate(int index) const override;
+	uint32_t translate(int index) const override;
 private:
 	const std::vector<int> pl;
 	const std::vector<int> ph;
