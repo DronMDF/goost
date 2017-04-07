ce4288a6d83a794f34ecfc168c83f25d6c951143
Andrey Valyaev <dron.valyaev@gmail.com>
2017-04-07 12:52:27 +0300

    #59: KeyIter3

--- kuznyechik/KeyIter.h
+++ kuznyechik/KeyIter.h
@@ -25,9 +25,8 @@ public:
 	explicit KeyIter3(const std::shared_ptr<const Key::Data> &key_data);
 	Block value() const override;
 private:
-	Block R(const Block &a) const;
+	Block S(const Block &a) const;
 	Block L(const Block &a) const;
-	Block LSX(const Block &a, const Block &c) const;
 
 	const std::unique_ptr<const Key::Iter> k1;
 	const std::unique_ptr<const Key::Iter> k2;
