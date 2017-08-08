cdc98948313cd5a2b34469df2a1a68fd6fab361a
Andrey Valyaev <dron.valyaev@gmail.com>
2017-08-08 23:47:24 +0300

    #142: style

--- test2o/kuznyechik/ReprKey.h
+++ test2o/kuznyechik/ReprKey.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2017 Andrey Valyaev
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+
 #pragma once
 #include <memory>
 #include <2out/Representation.h>
@@ -6,7 +11,7 @@
 // Representation of high key part.
 class ReprKeyHigh final : public oout::Representation {
 public:
-	ReprKeyHigh(const std::shared_ptr<kuznyechik::Key::Data> &key);
+	explicit ReprKeyHigh(const std::shared_ptr<kuznyechik::Key::Data> &key);
 	std::string asString() const override;
 private:
 	const std::shared_ptr<kuznyechik::Key::Data> key;
@@ -15,7 +20,7 @@ private:
 // Representation of low key part.
 class ReprKeyLow final : public oout::Representation {
 public:
-	ReprKeyLow(const std::shared_ptr<kuznyechik::Key::Data> &key);
+	explicit ReprKeyLow(const std::shared_ptr<kuznyechik::Key::Data> &key);
 	std::string asString() const override;
 private:
 	const std::shared_ptr<kuznyechik::Key::Data> key;
