3d573234536fa3eafdb5403ae3c8bd63da20e03a
Andrey Valyaev <dron.valyaev@gmail.com>
2017-11-06 22:56:40 +0300

    #108: magma::Stream interface

--- magma/DataStream.h
+++ magma/DataStream.h
@@ -7,18 +7,16 @@
 #include <cstdint>
 #include <memory>
 #include <vector>
+#include "Stream.h"
 
 namespace magma {
 
-class Iterator;
-
-// @todo #102:15min Introduce magma::Stream interface
-class DataStream : public std::enable_shared_from_this<const DataStream> {
+// @todo #108 Rename magma::DataStream to magma::StMemory
+class DataStream final : public Stream, public std::enable_shared_from_this<const DataStream> {
 public:
 	explicit DataStream(const std::vector<uint64_t> &data);
-
-	std::shared_ptr<const Iterator> iter(size_t offset = 0) const;
-
+	std::shared_ptr<const Iterator> iter() const override;
+	std::shared_ptr<const Iterator> next_iter(size_t offset = 0) const;
 private:
 	const std::vector<uint64_t> data;
 };
---
+++ magma/Stream.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2017 Andrey Valyaev
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+
+#pragma once
+
+namespace magma {
+
+class Iterator;
+
+class Stream {
+public:
+	virtual ~Stream() = default;
+	virtual std::shared_ptr<const Iterator> iter() const = 0;
+};
+
+}
