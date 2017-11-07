d52eef0e94a07e01843239782ec39d76a29789f7
Andrey Valyaev <dron.valyaev@gmail.com>
2017-11-07 13:51:41 +0300

    #310: Move DataStream test to 2out

---
+++ test2o/magma/StMemoryTest.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2017 Andrey Valyaev
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+
+#include <memory>
+#include <2out/Test.h>
+#include <2out/Result.h>
+
+namespace magma {
+
+class StMemoryTest final : public oout::Test {
+public:
+	StMemoryTest();
+	std::unique_ptr<const oout::Result> result() const override;
+private:
+	const std::shared_ptr<const oout::Test> tests;
+};
+
+}
+
