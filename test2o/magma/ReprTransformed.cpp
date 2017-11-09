f45dd57315affc264f68da9be9f7db62ee6cc551
Andrey Valyaev <dron.valyaev@gmail.com>
2017-11-09 23:22:05 +0300

    #312: SboxTwistedTest in 2out

---
+++ test2o/magma/ReprTransformed.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2017 Andrey Valyaev
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+
+#include "ReprTransformed.h"
+#include <sstream>
+#include <iomanip>
+#include <magma/Sbox.h>
+
+using namespace std;
+using namespace magma;
+
+ReprTransformed::ReprTransformed(const shared_ptr<const Sbox> &sbox, uint32_t value)
+	: sbox(sbox), value(value)
+{
+}
+
+string ReprTransformed::asString() const
+{
+	const auto result = sbox->transform(value);
+	ostringstream out;
+	out << hex << setfill('0') << setw(8) << result;
+	return out.str();
+}
--- test2o/magma/SboxTwistedTest.h
+++ test2o/magma/SboxTwistedTest.h
@@ -3,7 +3,6 @@
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
 
-#pragma once
 #include <memory>
 #include <2out/Test.h>
 #include <2out/Result.h>
@@ -12,9 +11,9 @@ namespace magma {
 
 class Sbox;
 
-class SboxNibbleTest final : public oout::Test {
+class SboxTwistedTest final : public oout::Test {
 public:
-	SboxNibbleTest();
+	SboxTwistedTest();
 	std::unique_ptr<const oout::Result> result() const override;
 private:
 	const std::shared_ptr<const Sbox> sbox;
