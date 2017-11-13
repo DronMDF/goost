9f3aae49c0127b3e9a4ea697bdf13539e2438377
Andrey Valyaev <dron.valyaev@gmail.com>
2017-11-13 22:48:21 +0300

    #349: BlkSwapped

--- test/magma/Test.cpp
+++ test/magma/Test.cpp
@@ -8,6 +8,7 @@
 #include "BlkDecryptedTest.h"
 #include "BlkEncryptedTest.h"
 #include "BlkStringTest.h"
+#include "BlkSwappedTest.h"
 #include "ImitTest.h"
 #include "KeyCachedDataTest.h"
 #include "KeyDataNativeTest.h"
@@ -28,6 +29,7 @@ magma::Test::Test()
 			make_shared<BlkDecryptedTest>(),
 			make_shared<BlkEncryptedTest>(),
 			make_shared<BlkStringTest>(),
+			make_shared<BlkSwappedTest>(),
 			make_shared<ImitTest>(),
 			make_shared<KeyCachedDataTest>(),
 			make_shared<KeyDataNativeTest>(),
