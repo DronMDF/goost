f9d8ab7289949a3ed5ffbf88178c6c557a7aed9c
Andrey Valyaev <dron.valyaev@gmail.com>
2018-04-22 14:00:38 +0300

    #372: test_package

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -3,11 +3,11 @@
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
 
-#include <iostream>
+#include <iomanip>
 #include <sstream>
-#include <kuznyechik/Imit.h>
-#include <kuznyechik/StMemory.h>
-#include <kuznyechik/Key.h>
+#include <goost/kuznyechik/Imit.h>
+#include <goost/kuznyechik/StMemory.h>
+#include <goost/kuznyechik/Key.h>
 
 using namespace std;
 using namespace kuznyechik;
@@ -34,7 +34,8 @@ int main(int, char **)
 	);
 
 	const auto value = imit->value();
+
 	ostringstream os;
 	os << hex << setfill('0') << setw(16) << value.second << setw(16) << value.first;
-	return os.str() == "336f4d296059fbe3" ? 0 : -1;
+	return os.str() == "336f4d296059fbe34ddeb35b37749c67" ? 0 : -1;
 }
