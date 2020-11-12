7455c58de5dfcc06ab6d631238190ef0fa141cdb
Andrey Valyaev <dron.valyaev@gmail.com>
2020-11-12 22:37:41 +0300

    #424: ECB algs implementation

---
+++ goost/magma/ECBSource.cpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2017-2020 Andrey Valyaev <dron.valyaev@gmail.com>
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+
+#include "ECBSource.h"
+#include <cstring>
+#include "BlkDecrypted.h"
+#include "BlkRaw.h"
+
+using namespace std;
+using namespace goost;
+using namespace goost::magma;
+
+ECBSource::ECBSource(
+	const shared_ptr<const Source> &source,
+	const shared_ptr<const Key> &key
+) : source(source), key(key)
+{
+}
+
+pair<vector<byte>, shared_ptr<const Source>> ECBSource::read(size_t size) const
+{
+	if (size < Block::size) {
+		throw runtime_error("Unaligned ECB decryption");
+	}
+
+	auto s = source;
+	vector<byte> p;
+	tie(p, s) = s->read(8);
+	if (p.empty()) {
+		// Decryption is done
+		return {p, s};
+	}
+
+	const auto e = BlkDecrypted(
+		make_shared<BlkRaw>(&p[0]),
+		key
+	).value();
+
+	memcpy(&p[0], &e.first, Block::size);
+
+	return {p, make_shared<ECBSource>(s, key)};
+}
---
+++ goost/magma/ECBSource.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2017-2020 Andrey Valyaev <dron.valyaev@gmail.com>
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+
+#pragma once
+#include <goost/Source.h>
+
+namespace goost {
+namespace magma {
+
+class Key;
+
+class ECBSource final : public Source {
+public:
+	ECBSource(
+		const std::shared_ptr<const Source> &source,
+		const std::shared_ptr<const Key> &key
+	);
+
+	std::pair<std::vector<std::byte>, std::shared_ptr<const Source>> read(
+		size_t size
+	) const override;
+
+private:
+	const std::shared_ptr<const Source> source;
+	const std::shared_ptr<const Key> key;
+};
+
+}
+}
